57d8d9317c2921749bd99766de7736a7c380fef2
ichane31 <roukeyaassouma@gmail.com>
2022-07-23 21:58:48 +0100

    first commit

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -10,7 +10,7 @@ export const config = {
 	CORS_ORIGIN: 'http://localhost:' + (process.env.PORT || 3000),
 	COOKIE_DOMAIN: 'localhost',
 	DB_URL: process.env.DATABASE_URL,
-	SESSION_SECRET: process.env.SESSION_SECRET||'',
+	//SESSION_SECRET: process.env.SESSION_SECRET||'',
 	EMAIL_USERNAME: process.env.EMAIL_USERNAME,
 	EMAIL_PASSWORD: process.env.EMAIL_PASSWORD
 };
