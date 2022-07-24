18414ab191e1c155c23f678c532191d95627c300
ichane31 <roukeyaassouma@gmail.com>
2022-07-24 15:13:05 +0100

    first commit

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -7,7 +7,7 @@ export const config = {
 	origin: 'https://projet-apis.herokuapp.com/',
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	JWT_SECRET: process.env.JWT_SECRET || '',
-	CORS_ORIGIN: 'http://localhost:' + (process.env.PORT || 3000),
+	//CORS_ORIGIN: 'http://localhost:' + (process.env.PORT || 3000),
 	COOKIE_DOMAIN: 'localhost',
 	DB_URL: process.env.DATABASE_URL,
 	//SESSION_SECRET: process.env.SESSION_SECRET||'',
