cd2cc47c38404c8293d5a499c836b2b5222f715d
ichane31 <roukeyaassouma@gmail.com>
2022-09-14 13:55:08 +0100

    update commit

--- src/middleware/ensureAuthenticated.middleware.ts
+++ src/middleware/ensureAuthenticated.middleware.ts
@@ -1,13 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import { UnauthorizedError } from '../error/UnauthorizedError.error';
-import { decodeUser } from './decodeuser.middleware';
+
 
 export const ensureAuthenticated = (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
-	decodeUser
 	if (req.currentUser?.userId) {
 		return next();
 	}
